115f62cbbb3c1ae7d3ea99ff72522ed9f0d77188
thiagoanjos <thiago.w3c@gmail.com>
2021-03-21 15:45:40 -0300

    lifecycle hooks, after create, after update

--- api/posts/models/posts.js
+++ api/posts/models/posts.js
@@ -1,8 +1,17 @@
-'use strict';
+"use strict";
+
+const axios = require("axios");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  async afterCreate(result, data) {
+    axios.post("https://api.netlify.com/build_hooks/60579400e133121256af8d27");
+  },
+  async afterUpdate(result, params, data) {
+    axios.post("https://api.netlify.com/build_hooks/60579400e133121256af8d27");
+  },
+};
